a01fed51e83b270cdd58e6397bb8db83d4987908
Александр Сенин <>
2023-09-27 12:14:47 +0400

    EZThreadSafetyKit: Add method update and projectedValue

--- Sources/EZThreadSafetyKit/EZThreadSafety.swift
+++ Sources/EZThreadSafetyKit/EZThreadSafety.swift
@@ -12,6 +12,8 @@ public class EZThreadSafety<T>: @unchecked Sendable{
     private let semaphore = DispatchSemaphore(value: 1)
     private var value: T
     
+    public var projectedValue: EZThreadSafety<T> { self }
+    
     public var wrappedValue: T{
         set(value){
             semaphore.wait(); defer { semaphore.signal() }
@@ -21,6 +23,11 @@ public class EZThreadSafety<T>: @unchecked Sendable{
             semaphore.wait(); defer { semaphore.signal() }
             return value
         }
+    }
+    
+    public func update(_ clusure: (inout T) -> ()){
+        semaphore.wait(); defer { semaphore.signal() }
+        clusure(&value)
     }
     
     public init(wrappedValue: T){
